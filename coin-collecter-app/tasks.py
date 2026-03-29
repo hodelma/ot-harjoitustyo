5af814d2ad3350bf287c609ade563ed95d105e5a
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-03-29 21:10:44 +0300

    Remove unnecessary invoke task

--- coin-collecter-app/tasks.py
+++ coin-collecter-app/tasks.py
@@ -1,21 +1,21 @@
 from invoke import task
 
+
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
 
 @task
 def test(ctx):
     ctx.run("pytest src/tests", pty=True)
 
+
 @task
 def build(ctx):
     ctx.run("python3 src/build.py", pty=True)
 
-@task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src/tests", pty=True)
 
-@task(coverage)
+@task
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)

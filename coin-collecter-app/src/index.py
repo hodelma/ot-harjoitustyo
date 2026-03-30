8f8036dfe1230330b078d5a29d8aeea0796fea63
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-03-30 17:40:50 +0300

    Initialize main application window and UI

--- coin-collecter-app/src/index.py
+++ coin-collecter-app/src/index.py
@@ -1,1 +1,17 @@
-#updated later
+from tkinter import Tk
+from ui.ui import UI
+
+
+def main():
+    """sets up and launches the main app window"""
+    window = Tk()
+    window.title("Coin Collector Game")
+
+    ui = UI(window)
+    ui.start()
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()

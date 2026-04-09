d5c0953f49a140bd04f1c17ac05e7fde5fc7d9a4
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-04-09 14:54:02 +0300

    Remove constant heights and widths

--- coin-collecter-app/src/sprites/coin.py
+++ coin-collecter-app/src/sprites/coin.py
@@ -3,8 +3,6 @@ import random
 import pygame
 
 dirname = os.path.dirname(__file__)
-SCREEN_WIDTH = 1250
-SCREEN_HEIGHT = 700
 
 
 class Coin(pygame.sprite.Sprite):
@@ -16,22 +14,22 @@ class Coin(pygame.sprite.Sprite):
         self.speed = 2
 
         if x is None:
-            self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+            self.rect.x = random.randint(0, 1250 - self.rect.width)
         else:
             self.rect.x = x
 
         if y is None:
-            self.rect.y = random.randint(-SCREEN_HEIGHT // 2, 0)
+            self.rect.y = random.randint(-1250 // 2, 0)
         else:
             self.rect.y = y
 
     def update(self):
         """moves coin downward and wraps if it reaches bottom"""
         self.rect.y += self.speed
-        if self.rect.y > SCREEN_HEIGHT:
+        if self.rect.y > 700:
             self.randomize_position()
 
     def randomize_position(self):
         """moves coin to a random start position at the top"""
-        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
-        self.rect.y = random.randint(-SCREEN_HEIGHT, -self.rect.height)
+        self.rect.x = random.randint(0, 1250 - self.rect.width)
+        self.rect.y = random.randint(-700, -self.rect.height)
--- coin-collecter-app/src/sprites/monster.py
+++ coin-collecter-app/src/sprites/monster.py
@@ -3,8 +3,6 @@ import random
 import pygame
 
 dirname = os.path.dirname(__file__)
-SCREEN_WIDTH = 1250
-SCREEN_HEIGHT = 700
 
 
 class Monster(pygame.sprite.Sprite):
@@ -17,21 +15,21 @@ class Monster(pygame.sprite.Sprite):
         self.speed = 3
 
         if x is None:
-            self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+            self.rect.x = random.randint(0, 1250 - self.rect.width)
         else:
             self.rect.x = x
 
         if y is None:
-            self.rect.y = random.randint(-SCREEN_HEIGHT // 2, -self.rect.height)
+            self.rect.y = random.randint(-700 // 2, -self.rect.height)
         else:
             self.rect.y = y
 
     def update(self):
         """move monster down and reset position"""
         self.rect.y += self.speed
-        if self.rect.y > SCREEN_HEIGHT:
+        if self.rect.y > 700:
             self.reset_position()
 
     def reset_position(self):
-        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
-        self.rect.y = random.randint(-SCREEN_HEIGHT, -self.rect.height)
+        self.rect.x = random.randint(0, 1250 - self.rect.width)
+        self.rect.y = random.randint(-700, -self.rect.height)

e0af01ee55b4a1c7ef51dd48788939e07adde236
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-04-09 12:43:54 +0300

    Initialize game

---
+++ coin-collecter-app/src/main.py
@@ -0,0 +1,28 @@
+import pygame
+from level import Level
+from game_loop import GameLoop
+from event_queue import EventQueue
+from renderer import Renderer
+from clock import Clock
+
+
+def main():
+    """initializes and starts the game"""
+    pygame.init()
+
+    display = pygame.display.set_mode((1250, 700))
+    pygame.display.set_caption("Coin Collector Game")
+
+    level = Level()
+    event_queue = EventQueue()
+    renderer = Renderer(display, level.all_sprites, level.game)
+    clock = Clock()
+
+    game_loop = GameLoop(level, renderer, event_queue, clock)
+    game_loop.start()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()

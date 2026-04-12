d88d776dd1a435a566f63eb469ed07a1c0acf22b
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-04-12 18:31:53 +0300

    Extend game_logic tests

--- coin-collecter-app/src/tests/test_game_logic.py
+++ coin-collecter-app/src/tests/test_game_logic.py
@@ -31,3 +31,37 @@ class TestGame(unittest.TestCase):
             self.game.collect_coin()
 
         self.assertEqual(self.game.has_won(), False)
+
+    def test_reset_gets_default_state_back(self):
+        self.game.score = 5
+        self.game.is_over = True
+        self.game.won = True
+        self.game.state = "game_over"
+        self.game.reset()
+
+        self.assertEqual(self.game.score, 0)
+        self.assertEqual(self.game.is_over, False)
+        self.assertEqual(self.game.won, False)
+        self.assertEqual(self.game.state, "playing")
+
+    def test_high_score_stays_after_reset(self):
+        for _ in range(3):
+            self.game.collect_coin()
+        self.game.reset()
+        self.assertEqual(self.game.high_score, 3)
+
+    def test_collect_coin_gives_win_at_20(self):
+        self.game.score = 19
+        self.game.collect_coin()
+
+        self.assertEqual(self.game.won, True)
+        self.assertEqual(self.game.is_over, True)
+        self.assertEqual(self.game.state, "game_over")
+
+    def test_hit_monster_sets_game_over(self):
+        self.game.collect_coin()
+        self.game.hit_monster()
+
+        self.assertEqual(self.game.is_over, True)
+        self.assertEqual(self.game.state, "game_over")
+        self.assertEqual(self.game.won, False)

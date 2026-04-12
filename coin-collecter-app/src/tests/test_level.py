9f7eb22c3b604595cfcb72cd25a460a06d5a2611
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-04-12 19:12:28 +0300

    Add collision and reset game stats test for level.py

--- coin-collecter-app/src/tests/test_level.py
+++ coin-collecter-app/src/tests/test_level.py
@@ -1,11 +1,9 @@
 import unittest
 from level import Level
 
-
 class FakeKeys:
     def __getitem__(self, key):
         return False
-
 
 class TestLevel(unittest.TestCase):
 
@@ -16,8 +14,37 @@ class TestLevel(unittest.TestCase):
         coin = list(level.coins)[0]
         coin.rect.center = level.player.rect.center
 
-        coin_count_before = len(level.coins)
+        coin_amount_before = len(level.coins)
         level.update(FakeKeys())
 
         self.assertEqual(level.game.score, 1)
-        self.assertEqual(len(level.coins), coin_count_before)
+        self.assertEqual(len(level.coins), coin_amount_before)
+
+    def test_monster_collision_is_game_over(self):
+        level = Level()
+        level.game.state = "playing"
+
+        monster = list(level.monsters)[0]
+        monster.rect.center = level.player.rect.center
+
+        level.update(FakeKeys())
+
+        self.assertEqual(level.game.score, 0)
+        self.assertEqual(level.game.is_over, True)
+        self.assertEqual(level.game.state, "game_over")
+        self.assertEqual(level.game.won, False)
+
+    def test_reset_resets_player_score_and_coins(self):
+        level = Level()
+        level.game.state = "playing"
+        level.game.score = 5
+        level.player.rect.x += 100
+        level.player.rect.y += 100
+
+        level.reset()
+
+        self.assertEqual(level.game.state, "playing")
+        self.assertEqual(level.game.score, 0)
+        self.assertEqual(level.player.rect.x, 1250 // 2)
+        self.assertEqual(level.player.rect.y, 700 // 2)
+        self.assertEqual(len(level.coins), 3)

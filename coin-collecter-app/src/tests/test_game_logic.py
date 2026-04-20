f78e0c73a36e6ca53b60e834265d1dd1729f2841
Heidi Tapani <heidi.tapani@helsinki.fi>
2026-04-20 12:28:33 +0300

    Update tests for game logic

--- coin-collecter-app/src/tests/test_game_logic.py
+++ coin-collecter-app/src/tests/test_game_logic.py
@@ -11,23 +11,46 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.score, 0)
 
     def test_collect_coin_increases_score(self):
-        self.game.collect_coin()
+        self.game.collect_coin(1)
         self.assertEqual(self.game.score, 1)
 
-    def test_hit_monster_resets_score(self):
-        self.game.collect_coin()
-        self.game.collect_coin()
+    def test_collect_coin_accumulates_different_values(self):
+        self.game.collect_coin(1)
+        self.game.collect_coin(2)
+        self.game.collect_coin(3)
+        self.assertEqual(self.game.score, 6)
+
+    def test_hit_monster_does_not_reset_score(self):
+        self.game.collect_coin(1)
         self.game.hit_monster()
-        self.assertEqual(self.game.score, 0)
+        self.assertEqual(self.game.score, 1)
+
+    def test_start_with_three_lives(self):
+        self.assertEqual(self.game.lives, 3)
+
+    def test_hit_monster_reduces_lives(self):
+        self.game.hit_monster()
+        self.assertEqual(self.game.lives, 2)
+
+    def test_hit_monster_once_does_not_set_game_over(self):
+        self.game.hit_monster()
+        self.assertEqual(self.game.is_over, False)
+
+    def test_hit_monster_three_times_sets_game_over(self):
+        self.game.hit_monster()
+        self.game.hit_monster()
+        self.game.hit_monster()
+        self.assertEqual(self.game.is_over, True)
+        self.assertEqual(self.game.state, "game_over")
 
     def test_has_won_returns_true_when_score_is_20(self):
         for _ in range(20):
-            self.game.collect_coin()
+            self.game.collect_coin(1)
         self.assertEqual(self.game.has_won(), True)
 
     def test_has_won_returns_false_when_score_is_less_than_20(self):
         for _ in range(10):
-            self.game.collect_coin()
+            self.game.collect_coin(1)
         self.assertEqual(self.game.has_won(), False)
 
     def test_reset_gets_default_state_back(self):
@@ -41,25 +64,17 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.is_over, False)
         self.assertEqual(self.game.won, False)
         self.assertEqual(self.game.state, "playing")
+        self.assertEqual(self.game.lives, 3)
 
     def test_high_score_stays_after_reset(self):
         for _ in range(3):
-            self.game.collect_coin()
+            self.game.collect_coin(1)
         self.game.reset()
         self.assertEqual(self.game.high_score, 3)
 
     def test_collect_coin_gives_win_at_20(self):
         self.game.score = 19
-        self.game.collect_coin()
-
+        self.game.collect_coin(1)
         self.assertEqual(self.game.won, True)
         self.assertEqual(self.game.is_over, True)
         self.assertEqual(self.game.state, "game_over")
-
-    def test_hit_monster_sets_game_over(self):
-        self.game.collect_coin()
-        self.game.hit_monster()
-
-        self.assertEqual(self.game.is_over, True)
-        self.assertEqual(self.game.state, "game_over")
-        self.assertEqual(self.game.won, False)
